2ad6033b8e4a05efbdc2990f858473e29f8262c1
Mani Teja <maniteja4m@gmail.com>
2024-03-11 02:51:49 +0530

    fixed actions and aded a route

---
+++ app/api/user/add/route.ts
@@ -0,0 +1,78 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+async function createUserTable() {
+	try {
+		// INSERT INTO Users (firstName, lastName, username, email, password) VALUES ("Maniteja", "Emmadi", "maniteja", "maniteja@test.com", "maniteja");
+		// CREATE TABLE IF NOT EXISTS Users ( firstName varchar(255), lastName varchar(255), username varchar(255) NOT NULL UNIQUE, email varchar(255) NOT NULL UNIQUE, password varchar(255) NOT NULL, id INT SERIAL PRIMARY KEY );
+		{
+			/* CREATE TABLE IF NOT EXISTS Users (
+      firstName VARCHAR(255),
+      lastName VARCHAR(255),
+      username VARCHAR(255) NOT NULL UNIQUE,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL,
+      id INT AUTO_INCREMENT PRIMARY KEY
+    ); */
+		}
+		const result = await sql`CREATE TABLE IF NOT EXISTS Users (
+      firstName VARCHAR(255),
+      lastName VARCHAR(255),
+      username VARCHAR(255) NOT NULL UNIQUE,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL,
+      id SERIAL PRIMARY KEY
+  );`;
+		return { message: "User table created" };
+	} catch (error) {
+		return { error: error };
+	}
+}
+
+export async function GET() {
+	try {
+		const result = await sql`SELECT * FROM Users;`;
+
+		return NextResponse.json({ result }, { status: 200 });
+	} catch (e) {
+		return NextResponse.json({ error: e }, { status: 500 });
+	}
+}
+
+export async function POST(request: Request) {
+	try {
+		const values = await request.json();
+
+		const result = await createUserTable();
+
+		try {
+			const existingUser =
+				await sql`SELECT * FROM Users WHERE email = ${values.email};`;
+
+			if (existingUser?.rows.length > 0) {
+				return NextResponse.json(
+					{
+						error: "User already exists",
+					},
+					{ status: 400 }
+				);
+			}
+
+			try {
+				const result =
+					await sql`INSERT INTO Users (firstName, lastName, username, email, password) VALUES (${values.firstName}, ${values.lastName}, ${values.username}, ${values.email}, ${values.password});`;
+
+				return NextResponse.json({ result }, { status: 200 });
+			} catch (error) {
+				return NextResponse.json({ error }, { status: 500 });
+			}
+		} catch (e) {
+			return NextResponse.json({ error: e }, { status: 500 });
+		}
+	} catch (e) {
+		return NextResponse.json(
+			{ error: JSON.stringify(e) || "Something went wrong" },
+			{ status: 500 }
+		);
+	}
+}
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,16 +1,3 @@
-import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
-
-const createUserTable = async () => {
-	try {
-		const result =
-			await sql`CREATE TABLE Users ( firstName varchar(255), lastName varchar(255), username varchar(255) NOT NULL UNIQUE, email varchar(255) NOT NULL UNIQUE, password varchar(255) NOT NULL, id INT NOT NULL SERIAL PRIMARY KEY );`;
-		return NextResponse.json({ result }, { status: 200 });
-	} catch (error) {
-		return NextResponse.json({ error }, { status: 500 });
-	}
-};
-
 export const addUser = async (values: {
 	firstName: string;
 	lastName: string;
@@ -19,29 +6,19 @@ export const addUser = async (values: {
 	password: string;
 }) => {
 	try {
-		const existingUser =
-			await sql`SELECT * FROM Users WHERE email = ${values.email};`;
-
-		try {
-			if (existingUser?.rows.length > 0) {
-				return NextResponse.json(
-					{
-						error: "User already exists",
-					},
-					{ status: 500 }
-				);
+		const result = await fetch(
+			`${process.env.NEXT_PUBLIC_SERVER_URL}/user/add`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(values),
 			}
+		).then((res) => res.json());
 
-			const result =
-				await sql`INSERT INTO Users (firstName, lastName, username, email, password) VALUES (${values.firstName}, ${values.lastName}, ${values.username}, ${values.email}, ${values.password});`;
-
-			return NextResponse.json({ result }, { status: 200 });
-		} catch (e) {
-			console.log(e);
-		}
+		return JSON.parse(JSON.stringify(result));
 	} catch (e) {
 		console.log(e);
-		await createUserTable();
-		addUser(values);
 	}
 };
